a6c806a2e2321fb29c3fee297cf4915a37b2cf48
dac114514 <dashuaicheng2@gmail.com>
2026-05-15 20:51:03 +0800

    build: add material-icons-extended, update XML resources to blue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
